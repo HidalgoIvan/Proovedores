cea1cc280669fb7b6fcb7dde72681749a411bbd6
HidalgoIvan <A01651425@itesm.mx>
2019-03-22 20:47:03 -0600

    mas clases

--- Envio/Cliente.java
+++ Envio/Cliente.java
@@ -2,9 +2,17 @@ public class Cliente
 {
 	private RFC rfc;
 	private int n;
-	String[] Datos;
+	String[] datos;
+	private DataAdapter dataAdapter;
+	public Cliente(RFC rfc, int n, String[] datos, DataAdapter dataAdapter)
+	{
+		this.rfc = rfc;
+		this.n = n;
+		this.datos = datos;
+		this.dataAdapter = dataAdapter;
+	}
 	public void generarEnvio(String datos, rfc)
 	{
-		
+		dataAdapter.generarEnvio(String[] datos, RFC rfc);
 	}
 }
--- Envio/RFC.java
+++ Envio/RFC.java
@@ -2,4 +2,9 @@ public class RFC
 {
 	private String direccionFiscal;
 	private Stirng razonSocial;
+	public RFC(String direccionFiscal, String razonSocial)
+	{
+		this.direccionFiscal = direccionFiscal;
+		this.razonSocial = razonSocial;
+	}
 }
