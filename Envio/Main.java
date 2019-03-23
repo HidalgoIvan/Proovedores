2f56498c9665a7bfee786f01660a1cdf66f4ade3
Rafael García Cadenas <rafaelgarciacadenas@gmail.com>
2019-03-22 21:08:01 -0600

    Main
    Main Casi vacio jaja

---
+++ Envio/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Bienvenidos a nuestro Package Program");
+        //RFC, Adaptador y cliente
+
+        rfc RFC =  new RFC("123", "Orden 4");
+        //adapter DataAdapter = new DataAdapter("");
+        //Cliente 
+
+    }
+}
